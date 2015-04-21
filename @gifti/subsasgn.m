6ec67a012d0b64a55a79790a92dd4be6152a31fe
gllmflndn <guillaume.flandin@gmail.com>
2015-04-21 18:16:15 +0100

    gifti v1.5

--- @gifti/subsasgn.m
+++ @gifti/subsasgn.m
@@ -4,11 +4,11 @@ function this = subsasgn(this, subs, A)
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
 % Guillaume Flandin
-% $Id: subsasgn.m 4848 2012-08-17 11:39:06Z guillaume $
+% $Id: subsasgn.m 6416 2015-04-21 15:34:10Z guillaume $
 
 switch subs(1).type
     case '.'
-        if ~ismember(subs(1).subs, {'vertices' 'faces' 'normals' 'cdata','mat','private'})
+        if ~ismember(subs(1).subs, {'vertices' 'faces' 'normals' 'cdata','mat','indices','private'})
             error('Reference to non-existent field ''%s''.',subs(1).subs);
         else
             % TODO % handle cases when length(subs) > 1
@@ -31,6 +31,9 @@ switch subs(1).type
                         this.data{n}.space.MatrixData = eye(4);
                     case 'faces'
                         in = 'NIFTI_INTENT_TRIANGLE';
+                        dt = 'NIFTI_TYPE_INT32';
+                    case 'indices'
+                        in = 'NIFTI_INTENT_NODE_INDEX';
                         dt = 'NIFTI_TYPE_INT32';
                     case 'normals'
                         in = 'NIFTI_INTENT_VECTOR';
@@ -58,7 +61,7 @@ switch subs(1).type
             elseif strcmp(subs(1).subs,'private')
                 this = builtin('subsasgn',this,subs(2:end),A);
             else
-                if strcmp(subs(1).subs,'faces')
+                if strcmp(subs(1).subs,'faces') || strcmp(subs(1).subs,'indices')
                     if length(subs) > 1
                         this.data{n}.data = int32(builtin('subsasgn',this.data{n}.data,subs(2:end),A-1));
                     else
@@ -67,11 +70,38 @@ switch subs(1).type
                     end
                 else
                     if length(subs) > 1
-                        this.data{n}.data = single(builtin('subsasgn',this.data{n}.data,subs(2:end),A));
-                        this.data{n}.attributes.Dim = size(this.data{n}.data);
+                        if numel(n) == 1
+                            this.data{n}.data = single(builtin('subsasgn',this.data{n}.data,subs(2:end),A));
+                            this.data{n}.attributes.Dim = size(this.data{n}.data);
+                        else
+                            if numel(subs(2).subs) == 1
+                                error('Linear indexing not supported: use multiple subscripts.');
+                            end
+                            idx = subs(2).subs{2};
+                            if isequal(idx,':'), idx = 1:numel(this.data); end
+                            for k=1:numel(idx)
+                                s = subs(2);
+                                s.subs{2} = 1;
+                                if numel(A) == 1
+                                    this.data{idx(k)}.data = single(builtin('subsasgn',this.data{idx(k)}.data,s,A));
+                                else
+                                    this.data{idx(k)}.data = single(builtin('subsasgn',this.data{idx(k)}.data,s,A(:,k)));
+                                end
+                                this.data{idx(k)}.attributes.Dim = size(this.data{idx(k)}.data);
+                            end
+                        end
                     else
-                        this.data{n}.data = single(A);
-                        this.data{n}.attributes.Dim = size(A);
+                        if numel(n) == 1
+                            if isa(A,'file_array')
+                                this.data{n}.data = A;
+                                this.data{n}.attributes.Dim = A.dim;
+                            else
+                                this.data{n}.data = single(A);
+                                this.data{n}.attributes.Dim = size(A);
+                            end
+                        else
+                            error('Syntax not implemented.');
+                        end
                     end
                 end
             end
